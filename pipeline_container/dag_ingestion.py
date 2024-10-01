b0b75686fd2c2c9da38199adf85372525f0f3e57
lucas.brossier <lucas.brossier@gmail.com>
2024-10-01 12:36:34 -0400

    clean code

--- pipeline_container/dag_ingestion.py
+++ pipeline_container/dag_ingestion.py
@@ -1,3 +1,32 @@
+"""
+data_ingestion_dag.py
+
+Ce module définit un Directed Acyclic Graph (DAG) pour l'ingestion de données et le
+modélisation avec Apache Airflow.
+
+Dépendances:
+- Apache Airflow: Ce module utilise les classes DAG, PythonOperator et BashOperator de 
+  la bibliothèque Airflow pour orchestrer les tâches.
+- La fonction `execute_data_ingestion` est importée depuis le module `data_ingestion`
+  et est utilisée pour ingérer des données.
+
+Description du DAG:
+- Le DAG est nommé "data_ingestion_dag" et est configuré pour s'exécuter quotidiennement 
+  à partir de la date de début spécifiée (29 septembre 2024).
+- Les paramètres par défaut du DAG incluent le propriétaire défini sur "airflow".
+  
+Tâches définies:
+1. `ingest_data_task`: Utilise le PythonOperator pour exécuter la fonction 
+   `execute_data_ingestion`, qui est responsable de l'ingestion des données.
+   
+2. `modeling_data_task`: Utilise le BashOperator pour exécuter une commande bash 
+   qui change le répertoire vers le projet DBT et exécute `dbt run`, utilisant 
+   les profils stockés dans le répertoire de modélisation d'Airflow.
+
+Les tâches sont dépendantes, où `ingest_data_task` doit être terminée avant que 
+`modeling_data_task` puisse commencer.
+"""
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
@@ -6,23 +35,17 @@ from datetime import datetime
 # Import de la fonction de ton script
 from data_ingestion import execute_data_ingestion
 
-default_args = {
-    "owner": "airflow",
-    "start_date": datetime(2024, 9, 29)
-}
+default_args = {"owner": "airflow", "start_date": datetime(2024, 9, 29)}
 
-with DAG("data_ingestion_dag",
-         default_args=default_args,
-         schedule="@daily") as dag:
+with DAG("data_ingestion_dag", default_args=default_args, schedule="@daily") as dag:
 
     ingest_data_task = PythonOperator(
-        task_id="ingest_data",
-        python_callable=execute_data_ingestion
+        task_id="ingest_data", python_callable=execute_data_ingestion
     )
 
     modeling_data_task = BashOperator(
         task_id="dbt_run",
-        bash_command="cd ${AIRFLOW_HOME}/modeling/dbt_project && dbt run --profiles-dir ${AIRFLOW_HOME}/modeling/"
+        bash_command="cd ${AIRFLOW_HOME}/modeling/dbt_project && dbt run --profiles-dir ${AIRFLOW_HOME}/modeling/",
     )
 
 ingest_data_task >> modeling_data_task
--- pipeline_container/data_ingestion.py
+++ pipeline_container/data_ingestion.py
@@ -1,13 +1,55 @@
+"""
+data_ingestion.py
+
+Ce module est responsable de l'ingestion des données à partir de
+sources externes, en particulier d'une API. Il contient des fonctions
+pour récupérer, traiter et stocker les données dans une base de données
+DuckDB.
+
+Fonctions disponibles :
+- get_all_data: Récupère toutes les données des types "tracks", 
+  "users" et "listen_history" depuis l'API.
+- get_duckdb_connection: Établit une connexion à la base de données
+  DuckDB spécifiée.
+- save_data_to_duckdb: Crée les tables nécessaires et insère les 
+  données récupérées dans DuckDB.
+- execute_data_ingestion: Fonction principale qui coordonne le 
+  processus d'ingestion de données en appelant les autres fonctions.
+
+Exemple d'utilisation :
+    Pour exécuter l'ingestion de données, appelez la fonction
+    execute_data_ingestion().
+"""
+
+
 import requests
 import duckdb
 
 # URL de base de l'API
 BASE_URL = "http://192.168.0.55:8000"
 
+
 # Fonction générique pour récupérer toutes les données depuis l'API
 def get_all_data():
+    """
+    Récupère toutes les données depuis l'API pour les types spécifiés.
+
+    Cette fonction effectue des requêtes à l'API pour récupérer les données
+    des types suivants : "tracks", "users", et "listen_history". Les données
+    sont paginées, et la fonction continue à faire des requêtes jusqu'à ce
+    que toutes les pages soient récupérées.
+
+    Renvoie :
+        dict: Un dictionnaire contenant les données récupérées, où chaque
+        clé est le type de données et la valeur est une liste d'éléments.
+        Par exemple, {"tracks": [...], "users": [...], "listen_history": [...]}
+
+    Exemple d'utilisation :
+        data = get_all_data()
+    """
+
     data = {}
-    for data_type in ['tracks', 'users', 'listen_history']:
+    for data_type in ["tracks", "users", "listen_history"]:
         page = 1
         items = []
         while True:
@@ -15,26 +57,69 @@ def get_all_data():
             response = requests.get(url, params={"page": page, "size": 100}, timeout=10)
             if response.status_code == 200:
                 data_chunk = response.json()
-                items.extend(data_chunk.get('items', []))
-                if len(data_chunk.get('items', [])) < 100:  # Si moins de 100 items, on est à la dernière page
+                items.extend(data_chunk.get("items", []))
+                if (
+                    len(data_chunk.get("items", [])) < 100
+                ):  # Si moins de 100 items, on est à la dernière page
                     break
                 page += 1
             else:
-                print(f"Erreur lors de la récupération des {data_type}: {response.status_code}")
+                print(
+                    f"Erreur lors de la récupération des {data_type}: {response.status_code}"
+                )
                 break
         data[data_type] = items
     return data
 
+
 # Connexion à DuckDB
 def get_duckdb_connection():
-    return duckdb.connect('data/moovitamix.db')
+    """
+    Établit une connexion à la base de données DuckDB.
+
+    Cette fonction crée et renvoie une connexion à la base de données 
+    DuckDB spécifiée dans le chemin. Si la base de données n'existe pas, 
+    elle sera créée.
+
+    Renvoie:
+        duckdb.DuckDBPyConnection: Un objet de connexion DuckDB.
+
+    Exemple d'utilisation:
+        conn = get_duckdb_connection()
+    """
+
+    return duckdb.connect("data/moovitamix.db")
+
 
 # Fonction pour créer les tables et insérer les données dans DuckDB
 def save_data_to_duckdb(data):
+    """
+    Crée les tables nécessaires et insère les données dans DuckDB.
+
+    Cette fonction prend un dictionnaire de données récupérées et les
+    enregistre dans des tables appropriées dans la base de données 
+    DuckDB. Les tables créées comprennent "tracks", "users" et 
+    "listen_history". La fonction gère également les erreurs d'insertion 
+    et imprime des messages d'erreur si nécessaire.
+
+    Paramètres:
+        data (dict): Un dictionnaire contenant les données à insérer,
+                     avec des clés pour "tracks", "users" et 
+                     "listen_history".
+
+    Renvoie:
+        None: Cette fonction ne renvoie rien.
+
+    Exemple d'utilisation:
+        data = get_all_data()  # Récupérer les données depuis l'API
+        save_data_to_duckdb(data)  # Enregistrer les données dans DuckDB
+    """
     con = get_duckdb_connection()
 
+
     # Création des tables
-    con.execute('''
+    con.execute(
+        """
         CREATE TABLE IF NOT EXISTS tracks (
             id VARCHAR,
             name VARCHAR,
@@ -46,8 +131,10 @@ def save_data_to_duckdb(data):
             created_at TIMESTAMP,
             updated_at TIMESTAMP
         )
-    ''')
-    con.execute('''
+    """
+    )
+    con.execute(
+        """
         CREATE TABLE IF NOT EXISTS users (
             id VARCHAR,
             first_name VARCHAR,
@@ -58,83 +145,123 @@ def save_data_to_duckdb(data):
             created_at TIMESTAMP,
             updated_at TIMESTAMP
         )
-    ''')
-    con.execute('''
+    """
+    )
+    con.execute(
+        """
         CREATE TABLE IF NOT EXISTS listen_history (
             user_id VARCHAR,
             items INTEGER,
             created_at TIMESTAMP
         )
-    ''')
+    """
+    )
 
     # Insertion des tracks
-    if data.get('tracks'):
-        for track in data['tracks']:
+    if data.get("tracks"):
+        for track in data["tracks"]:
             if isinstance(track, dict):
                 try:
-                    con.execute('''
-                        INSERT INTO tracks (id, name, artist, songwriters, duration, genres, album, created_at, updated_at)
+                    con.execute(
+                        """
+                        INSERT INTO tracks (
+                        id, 
+                        name, 
+                        artist, 
+                        songwriters, 
+                        duration, 
+                        genres, 
+                        album, 
+                        created_at, 
+                        updated_at
+                        )
                         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-                    ''', (
-                        track.get('id'),
-                        track.get('name'),
-                        track.get('artist'),
-                        track.get('songwriters'),
-                        track.get('duration'),
-                        track.get('genres'),
-                        track.get('album'),
-                        track.get('created_at'),
-                        track.get('updated_at')
-                    ))
+                    """,
+                        (
+                            track.get("id"),
+                            track.get("name"),
+                            track.get("artist"),
+                            track.get("songwriters"),
+                            track.get("duration"),
+                            track.get("genres"),
+                            track.get("album"),
+                            track.get("created_at"),
+                            track.get("updated_at"),
+                        ),
+                    )
                 except Exception as e:
-                    print(f"Erreur lors de l'insertion des données des tracks pour l'ID {track.get('id')} : {e}")
+                    print(
+                        f"Erreur d'insertion pour l'ID {track.get('id')} : {e}"
+                    )
 
     # Insertion des utilisateurs
-    if data.get('users'):
-        for user in data['users']:
+    if data.get("users"):
+        for user in data["users"]:
             if isinstance(user, dict):
                 try:
-                    con.execute('''
-                        INSERT INTO users (id, first_name, last_name, email, gender, favorite_genres, created_at, updated_at)
+                    con.execute(
+                        """
+                        INSERT INTO users (
+                        id, 
+                        first_name, 
+                        last_name, 
+                        email, 
+                        gender, 
+                        favorite_genres, 
+                        created_at, 
+                        updated_at
+                        )
                         VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-                    ''', (
-                        user.get('id'),
-                        user.get('first_name'),
-                        user.get('last_name'),
-                        user.get('email'),
-                        user.get('gender'),
-                        user.get('favorite_genres'),
-                        user.get('created_at'),
-                        user.get('updated_at')
-                    ))
+                    """,
+                        (
+                            user.get("id"),
+                            user.get("first_name"),
+                            user.get("last_name"),
+                            user.get("email"),
+                            user.get("gender"),
+                            user.get("favorite_genres"),
+                            user.get("created_at"),
+                            user.get("updated_at"),
+                        ),
+                    )
                 except Exception as e:
-                    print(f"Erreur lors de l'insertion des données des utilisateurs pour l'ID {user.get('id')} : {e}")
+                    print(
+                        f"Erreur d'insertion pour l'ID {user.get('id')} : {e}"
+                    )
 
     # Insertion de l'historique d'écoute avec prise en charge de 'items' en tant qu'entier
-    if data.get('listen_history'):
-        for record in data['listen_history']:
+    if data.get("listen_history"):
+        for record in data["listen_history"]:
             if isinstance(record, dict):
-                user_id = record.get('user_id')
-                items = record.get('items', [])  # Utilisation du champ 'items' qui est une liste
-                created_at = record.get('created_at')  # Utilisation de 'created_at' comme timestamp d'écoute
-
-                print(f"Utilisateur: {user_id}, Items: {items}, Created At: {created_at}")
+                user_id = record.get("user_id")
+                items = record.get(
+                    "items", []
+                )  # Utilisation du champ 'items' qui est une liste
+                created_at = record.get(
+                    "created_at"
+                )  # Utilisation de 'created_at' comme timestamp d'écoute
 
                 # Insertion de chaque item de la liste 'items'
                 for item in items:
-                    if isinstance(item, (int, str)):  # Vérifie que 'item' est bien un 'track_id'
+                    if isinstance(
+                        item, (int, str)
+                    ):  # Vérifie que 'item' est bien un 'track_id'
                         try:
-                            con.execute('''
+                            con.execute(
+                                """
                                 INSERT INTO listen_history (user_id, items, created_at)
                                 VALUES (?, ?, ?)
-                            ''', (
-                                user_id,
-                                int(item),  # 'item' est maintenant un entier
-                                created_at
-                            ))
+                            """,
+                                (
+                                    user_id,
+                                    int(item),  # 'item' est maintenant un entier
+                                    created_at,
+                                ),
+                            )
                         except Exception as e:
-                            print(f"Erreur lors de l'insertion de l'historique d'écoute pour l'utilisateur {user_id} et le track {item} : {e}")
-
+                            print(
+                                f"Erreur d'insertion pour l'ID {user_id} et le track {item} : {e}"
+                            )
 
     con.close()
     print("Toutes les données stockées dans DuckDB")
@@ -142,5 +269,19 @@ def save_data_to_duckdb(data):
 
 # Fonction principale à appeler depuis Airflow
 def execute_data_ingestion():
+    """
+    Fonction principale pour exécuter l'ingestion des données.
+
+    Cette fonction récupère toutes les données depuis l'API et les
+    enregistre dans la base de données DuckDB. Elle coordonne les
+    appels aux fonctions de récupération et de sauvegarde des données.
+
+    Renvoie:
+        None: Cette fonction ne renvoie rien.
+
+    Exemple d'utilisation:
+        execute_data_ingestion()
+    """
+
     data = get_all_data()
     save_data_to_duckdb(data)
